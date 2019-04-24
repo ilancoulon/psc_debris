4771bbf42e325b9f22d3c639cd8862a774dd7f69
Ilan Coulon <ilan.coulon@gmail.com>
2019-04-24 18:38:36 +0200

    githubpages

---
+++ docs/fetch.js
@@ -0,0 +1,234 @@
+var debris;
+var ourSat;
+var moons;
+var ourMoon;
+var trajectories;
+var ourTrajectory;
+var risks;
+
+var numOfDebris = 1600;
+
+const ourSatId = 2681;
+const mostDangerousDebId = 2858;
+var timeOfCollision;
+
+const sizeOfDebris = 30;
+
+var speedRatio = 100;
+var refreshingRate = 0.05;//s
+
+function drawMoons() {
+  calcTraj();
+  calculateRisks();
+  drawOurLine();
+  var detail = detailRisk(mostDangerousDebId);
+  timeOfCollision = detail.tcol - 2;
+  moons = [];
+
+
+  var geometry2 = new THREE.SphereBufferGeometry( (sizeOfDebris+30)*ratioRealToSphere, 16, 16 );
+  var material2 = new THREE.MeshBasicMaterial();
+  ourMoon = new THREE.Mesh( geometry2, material2 );
+  scene.add( ourMoon );
+
+  for (var i = 0; i < numOfDebris-1; i++) {
+    var geometry = new THREE.SphereBufferGeometry( sizeOfDebris*ratioRealToSphere, 16, 16 );
+    var material = new THREE.MeshBasicMaterial();
+    if (i == mostDangerousDebId) {
+      material.color = new THREE.Color(0,0,1);
+    } else {
+      material.color = new THREE.Color(risks[i],1-risks[i],0);
+    }
+    moon = new THREE.Mesh( geometry, material );
+    scene.add( moon );
+    moons.push(moon);
+
+  }
+
+  initRender();
+}
+
+function calcTraj() {
+  trajectories = [];
+
+  var nowDate = moment("2019-04-23 12:30").toDate();
+  var nowJ = jday(nowDate.getUTCFullYear(),
+               nowDate.getUTCMonth() + 1,
+               nowDate.getUTCDate(),
+               nowDate.getUTCHours(),
+               nowDate.getUTCMinutes(),
+               nowDate.getUTCSeconds());
+  nowJ += nowDate.getUTCMilliseconds() * 1.15741e-8; //days per millisecond
+
+
+  for (var i = 0; i < debris.length; i++) {
+    trajectories[i] = [];
+    var now = (nowJ - debris[i].jdsatepoch) * 1440.0; //in minutes
+    for(var t=0; t < tFinal; t += timeDelta){
+      var aux = satellite.sgp4(debris[i], now + (t/60 * refreshingRate * speedRatio));
+
+      if (!aux.position || !aux.velocity) {
+        aux = {
+          position: {
+            x: 0,
+            y: 0,
+            z: 0
+          },
+          velocity: {
+            x: 0,
+            y: 0,
+            z: 0
+          }
+        }
+      }
+
+      trajectories[i].push({
+        position: {
+          x: ratioRealToSphere * aux.position.x,
+          y: ratioRealToSphere * aux.position.y,
+          z: ratioRealToSphere * aux.position.z
+        },
+        velocity: {
+          x: ratioRealToSphere * aux.velocity.x,
+          y: ratioRealToSphere * aux.velocity.y,
+          z: ratioRealToSphere * aux.velocity.z
+        }
+      });
+    }
+  }
+  var now = (nowJ - ourSat.jdsatepoch) * 1440.0; //in minutes
+
+  ourTrajectory = [];
+  for(var t=0; t < tFinal; t += timeDelta){
+    var aux = satellite.sgp4(ourSat,now + t/60 * refreshingRate * speedRatio);
+    ourTrajectory.push({
+      position: {
+        x: ratioRealToSphere * aux.position.x,
+        y: ratioRealToSphere * aux.position.y,
+        z: ratioRealToSphere * aux.position.z
+      },
+      velocity: {
+        x: ratioRealToSphere * aux.velocity.x,
+        y: ratioRealToSphere * aux.velocity.y,
+        z: ratioRealToSphere * aux.velocity.z
+      }
+    });
+  }
+}
+
+function calculateRisks() {
+  risks = [];
+  var maxRisk = 0;
+  var maxRiskId = 0;
+  for (var i = 0; i < trajectories.length; i++) {
+    risks.push(calculateRisk(i));
+    if (calculateRisk(i) > maxRisk) {
+      maxRisk = calculateRisk(i);
+      maxRiskId = i;
+    }
+  }
+}
+
+function dist(a,b){
+    return ((a.x-b.x)**2+(a.y-b.y)**2+(a.z-b.z)**2)**0.5
+}
+
+function calculateRisk(i) {
+  var dmin=dist(ourTrajectory[0].position,trajectories[i][0].position);
+  var jcol=0;
+  for(var j=1;j<trajectories[i].length;j++) {
+    var distancej = dist(ourTrajectory[j].position, trajectories[i][j].position);
+    if(distancej < dmin){
+      dmin  =distancej;
+      jcol = j;
+    }
+  }
+  var vcol=dist(ourTrajectory[jcol].velocity, trajectories[i][jcol].velocity);
+  var tcol=jcol*timeDelta+1; //in minutes
+  return 100  * vcol**0.3 / (dmin**2 * (tcol**0.2));
+}
+function detailRisk(i) {
+  var dmin=dist(ourTrajectory[0].position,trajectories[i][0].position);
+  var jcol=0;
+  for(var j=1;j<trajectories[i].length;j++) {
+    var distancej = dist(ourTrajectory[j].position, trajectories[i][j].position);
+    if(distancej < dmin){
+      dmin  =distancej;
+      jcol = j;
+    }
+  }
+  var vcol=dist(ourTrajectory[jcol].velocity, trajectories[i][jcol].velocity);
+  var tcol=jcol*timeDelta+1; //in minutes
+  return {
+    risk: 100 / (dmin/(vcol**0.2)*(tcol**0.3)),
+    dmin: dmin,
+    vcol: vcol,
+    jcol: jcol,
+    tcol: tcol
+  };
+}
+function findClosest() {
+  var maxRisk = 0;
+  var curRisk = 0;
+  var maxSat = 0;
+  var maxDeb = 0;
+  for (var i = 0; i < trajectories.length; i++) {
+    console.log("Oursat : "+i);
+    ourTrajectory = trajectories[i];
+    for (var j = i+1; j < trajectories.length; j++) {
+      curRisk = calculateRisk(j);
+      if(curRisk > maxRisk) {
+        maxRisk = curRisk;
+        maxSat = i;
+        maxDeb = j;
+      }
+    }
+  }
+  return {
+    maxRisk: maxRisk,
+    maxSat: maxSat,
+    maxDeb: maxDeb
+  }
+}
+
+function fetchTLE() {
+  console.log("Fetching TLE from spacetrack...");
+  const query = 'https://www.space-track.org/basicspacedata/query/class/tle_latest/ORDINAL/1/EPOCH/%3Enow-30/MEAN_MOTION/%3E11.25/ECCENTRICITY/%3C0.25/OBJECT_TYPE/payload/orderby/NORAD_CAT_ID/format/tle';
+  var url = new URL("/tle.txt");
+  fetch(url, {
+    method: 'POST'
+  })
+  .then(res => res.text())
+  .then(
+    (result) => {
+      console.log("TLEs fetched !");
+      const tles = result.split('\n');
+      numOfDebris = tles.length/2 - 1;
+      ourSat = satellite.twoline2satrec(tles[0], tles[1]);
+      debris = [];
+      for (var i = 2; i < numOfDebris*2; i+=2) {
+        const satrec = satellite.twoline2satrec(tles[i], tles[i+1]);
+        debris.push(satrec);
+      }
+      ourSat = debris[ourSatId];
+      drawMoons();
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+}
+
+fetchTLE();
+
+function jday(year, mon, day, hr, minute, sec){ //from satellite.js
+  'use strict';
+  return (367.0 * year -
+        Math.floor((7 * (year + Math.floor((mon + 9) / 12.0))) * 0.25) +
+        Math.floor( 275 * mon / 9.0 ) +
+        day + 1721013.5 +
+        ((sec / 60.0 + minute) / 60.0 + hr) / 24.0  //  ut in days
+        //#  - 0.5*sgn(100.0*year + mon - 190002.5) + 0.5;
+        );
+}
---
+++ docs/trajectory.js
@@ -0,0 +1,219 @@
+var trajectory;
+
+var container, stats;
+var camera, scene, renderer;
+var group;
+var mouseX = 0, mouseY = 0;
+var moon;
+var canvas;
+var controls;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+
+var earthSphereRadius = 200;
+var earthRealRadius = 6371;
+var ratioRealToSphere = earthSphereRadius / earthRealRadius;
+
+var timeDelta=1; //timeDelta in minutes
+var tFinal=2000; // duration until end in minutes also
+
+var movingDebris = true;
+var cameraOnOurSat = 0;
+
+init();
+
+function init() {
+  scene = new THREE.Scene();
+
+  group = new THREE.Group();
+  scene.add( group );
+
+  // earth
+
+  var loader = new THREE.TextureLoader();
+  loader.load( 'three.js/examples/textures/land_ocean_ice_cloud_2048.jpg', function ( texture ) {
+    var geometry = new THREE.SphereBufferGeometry( earthSphereRadius, 64, 64 );
+
+    var material = new THREE.MeshBasicMaterial( {
+      map: texture
+    } );
+    var mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+  } );
+
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera.position.z = 1000;
+
+  controls = new THREE.OrbitControls( camera );
+
+  document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function initRender() {
+  container = document.getElementById("container");
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  container.appendChild( renderer.domElement );
+
+  stats = new Stats();
+  container.appendChild( stats.dom );
+  animate();
+  moveDebris();
+}
+
+function onWindowResize() {
+
+  windowHalfX = window.innerWidth / 2;
+  windowHalfY = window.innerHeight / 2;
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+  mouseX = ( event.clientX - windowHalfX );
+  mouseY = ( event.clientY - windowHalfY );
+
+}
+
+//
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  render();
+  stats.update();
+
+}
+
+
+var positionIndex = 0;
+
+function toggleMovingDebris() {
+  if (movingDebris) {
+    movingDebris = false;
+  } else {
+    movingDebris = true;
+  }
+}
+document.getElementById('buttonMovingDebris').addEventListener('click', toggleMovingDebris);
+
+function toggleCamera() {
+  if (cameraOnOurSat == 2) {
+    cameraOnOurSat = 0;
+  } else {
+    cameraOnOurSat++;
+  }
+}
+document.getElementById('buttonCameraOnOurSat').addEventListener('click', toggleCamera);
+
+function moveDebris() {
+  setTimeout(moveDebris, refreshingRate*1000);
+  if(movingDebris) {
+    if (typeof positionIndex !== 'undefined' &&typeof moon !== 'undefined') {
+      if (positionIndex == timeOfCollision) {
+        toggleMovingDebris();
+      }
+
+      for (var i = 0; i < numOfDebris; i++) {
+        if (i != ourSatId && typeof debris[i] !== 'undefined' && typeof moons[i] !== 'undefined') {
+          moons[i].position.x = trajectories[i][positionIndex].position.x;
+          moons[i].position.y = trajectories[i][positionIndex].position.y;
+          moons[i].position.z = trajectories[i][positionIndex].position.z;
+        }
+      }
+      ourMoon.position.x = ourTrajectory[positionIndex].position.x;
+      ourMoon.position.y = ourTrajectory[positionIndex].position.y;
+      ourMoon.position.z = ourTrajectory[positionIndex].position.z;
+
+      document.getElementById("timeSpan").textContent = positionIndex;
+
+      if (positionIndex < tFinal/timeDelta - 2) {
+        positionIndex += 1;
+      } else {
+        positionIndex = 0;
+      }
+    }
+  }
+
+}
+
+function render() {
+
+  group.rotation.y -= 0.005;
+  dealWithCam();
+
+  renderer.render( scene, camera );
+
+}
+
+function dealWithCam() {
+  if (cameraOnOurSat == 0) {
+    const ourPos = ourTrajectory[positionIndex].position;
+    const ourVel = ourTrajectory[positionIndex].velocity;
+    camera.position.x = ourPos.x*1.1 - ourVel.x*200;
+    camera.position.y = ourPos.y*1.1 - ourVel.y*200;
+    camera.position.z = ourPos.z*1.1 - ourVel.z*200;
+    camera.lookAt( new THREE.Vector3(
+      ourPos.x,
+      ourPos.y,
+      ourPos.z
+    ) );
+  } else if(cameraOnOurSat == 1) {
+    const ourPos = ourTrajectory[positionIndex].position;
+    const ourVel = ourTrajectory[positionIndex].velocity;
+    camera.position.x = ourPos.x*1.5;
+    camera.position.y = ourPos.y*1.5;
+    camera.position.z = ourPos.z*1.5;
+    camera.lookAt( new THREE.Vector3(
+      ourPos.x,
+      ourPos.y,
+      ourPos.z
+    ) );
+  } else {
+    controls.update();
+
+    camera.lookAt( scene.position );
+  }
+}
+
+function getTrajectory() {
+
+}
+
+function drawOurLine() {
+  var i = 0;
+  while(dist(ourTrajectory[0].position, ourTrajectory[i].position) < 5) {
+    i++;
+  }
+  while(dist(ourTrajectory[0].position, ourTrajectory[i].position) > 1) {
+    i++;
+  }
+  const numberOfSegments = 250;
+  const numberForOneTurn = i + numberOfSegments + 1;
+  const step = Math.floor(numberForOneTurn / numberOfSegments);
+  var material = new THREE.LineBasicMaterial( {
+    color: 0xffffff
+  } );
+  var geometry = new THREE.Geometry();
+  for (var i = 0; i < numberOfSegments; i++) {
+    geometry.vertices.push(new THREE.Vector3(
+      ourTrajectory[i * step].position.x,
+      ourTrajectory[i * step].position.y,
+      ourTrajectory[i * step].position.z
+    ) );
+  }
+  var line = new THREE.Line( geometry, material );
+  scene.add(line);
+  console.log(line);
+}
